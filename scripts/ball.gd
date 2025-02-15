93c4c984720d88ebd496e5fe802d799623e5b87a
Lucas Pacheco <lucaslfp.00@gmail.com>
2025-02-15 19:55:31 -0300

    feat: ball bounce feature

---
+++ scripts/ball.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var y_speed = -250
+@export var angle = [-200, -250, 250, 300]
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("start") and !GameManager.started:
+		play_game()
+		
+	if GameManager.started:
+		var collision = move_and_collide(velocity*delta)
+		if collision != null:
+			velocity = velocity.bounce(collision.get_normal())
+
+func play_game():
+	GameManager.started = true
+	velocity = Vector2(angle.pick_random(), y_speed)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var ball: CharacterBody2D = $"."
+
 
 func _physics_process(delta: float) -> void:
 	velocity.x = 0
 	
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") and GameManager.started:
 		velocity.x -= speed
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right") and GameManager.started:
 		velocity.x += speed
 		
 	move_and_collide(velocity * delta)
