f126e1b0c6d60bc50224cc2bb03bff0878c2ef83
Lucas Pacheco <lucaslfp.00@gmail.com>
2025-02-15 20:16:39 -0300

    feat: add script to instantiate and generate power up items

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var y_speed = -250
 @export var angle = [-200, -250, 250, 300]
+@export var drop_probability = 0.2
+@export var item_scene = preload("res://item_power_up.tscn")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("start") and !GameManager.started:
@@ -16,7 +18,16 @@ func _physics_process(delta: float) -> void:
 			
 			if "Block" in collided_object.name:
 				collided_object.queue_free() 
+				spawn_item(collision.get_position())
 
 func play_game():
 	GameManager.started = true
 	velocity = Vector2(angle.pick_random(), y_speed)
+	
+func spawn_item(position_item: Vector2):
+	if randf() < drop_probability:
+		var item = item_scene.instantiate()
+		
+		item.position = position_item
+		get_parent().add_child(item)
+	
