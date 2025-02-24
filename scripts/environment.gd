b8fa7e29b92677b49c4f8c5d9b9eb8ff8d064744
Lucas Pacheco <lucaslfp.00@gmail.com>
2025-02-23 22:09:06 -0300

    feat: remove a body when it enters death zone, except if its the ball

--- scripts/environment.gd
+++ scripts/environment.gd
@@ -2,20 +2,15 @@ extends Node2D
 
 @onready var timer_reset: Timer = $TimerReset
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_death_zone_body_entered(body: Node2D) -> void:
 	if "Ball" in body.name:
 		timer_reset.start()
+	else: 
+		body.queue_free()
+	
 
 
 func _on_timer_reset_timeout() -> void:
+	GameManager.started = false
 	get_tree().reload_current_scene()
